f7c7f9877eda2a453f0e42350903b7655bff8ffd
fasihh <fhk.tcs@gmail.com>
2025-03-15 18:20:50 +0500

    Refactor PostController methods for improved clarity and consistency; update exception handling for user creation.

--- src/api/controllers/post.ts
+++ src/api/controllers/post.ts
@@ -5,12 +5,12 @@ import { ExceptionType } from "../errors/exceptions";
 import { deleteFileFromCloudinary } from "../utils/cloudinary";
 
 class PostController {
-    async getAll(req: Request, res: Response) {
+    async getAll(_req: Request, res: Response) {
         res.status(200).json({
             success: true,
-            message: "Fetched posts successfully",
+            message: "Fetched posts successfully.",
             post: await PostService.findAll(),
-        })
+        });
     }
 
     async getById(req: Request, res: Response) {
@@ -18,7 +18,7 @@ class PostController {
 
         res.status(200).json({
             success: true,
-            message: "Community fetched successfully",
+            message: "Post fetched successfully.",
             post: await PostService.findById(id),
         });
     }
@@ -27,54 +27,48 @@ class PostController {
         const communityId = req.params.communityId
         const title = req.body.title
         const content = req.body.content
-        const files = req.files as Express.Multer.File[];
         const creatorId = req.user.userId
 
-        if (!communityId || !title || !content) {
+        if (!title || !content)
             throw new RequestError(ExceptionType.INVALID_REQUEST);
-        }
 
-        const fileData = files?.map(file => ({
-            asset_id: (file as any).asset_id,
-            public_id: (file as any).public_id,
-            url: (file as any).url,
-            secure_url: (file as any).secure_url,
-            format: (file as any).format,
-            resource_type: (file as any).resource_type,
-            bytes: (file as any).bytes,
+        const fileData = req.files?.map(file => ({
+            asset_id: file.asset_id,
+            public_id: file.public_id,
+            url: file.url,
+            secure_url: file.secure_url,
+            format: file.format,
+            resource_type: file.resource_type,
+            bytes: file.bytes,
             original_filename: file.originalname
         })) || [];
 
-        
         await PostService.create(communityId, title, content, creatorId, fileData);
 
-        res.status(200).json({
+        res.status(201).json({
             success: true,
-            message: "Post created successfully",
-        })
+            message: "Post created successfully.",
+        });
     }
 
     async updateById(req: Request, res: Response) {
         const id = req.params.id;
         const post = await PostService.findById(id);
 
-        if (!post) {
+        if (!post)
             throw new RequestError(ExceptionType.NOT_FOUND);
-        }
 
-        if (post.creatorId.toString() !== req.user.id) {
+        if (post.creatorId.toString() !== req.user.id)
             throw new RequestError(ExceptionType.UNAUTHORIZED);
-        }
 
-        const files = req.files as Express.Multer.File[];
-        const fileData = files?.map(file => ({
-            asset_id: (file as any).asset_id,
-            public_id: (file as any).public_id,
-            url: (file as any).url,
-            secure_url: (file as any).secure_url,
-            format: (file as any).format,
-            resource_type: (file as any).resource_type,
-            bytes: (file as any).bytes,
+        const fileData = req.files?.map(file => ({
+            asset_id: file.asset_id,
+            public_id: file.public_id,
+            url: file.url,
+            secure_url: file.secure_url,
+            format: file.format,
+            resource_type: file.resource_type,
+            bytes: file.bytes,
             original_filename: file.originalname
         })) || [];
 
@@ -120,8 +114,8 @@ class PostController {
 
         res.status(200).json({
             success: true,
-            message: 'Post deleted successfully'
-        })
+            message: 'Post deleted successfully.'
+        });
     }
 }
 
